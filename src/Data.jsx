8dfcc0bb4bbff1b7f72b77de78e4c139fb97ab49
unknown <email>
2023-02-03 11:36:15 -0300

    correção das images dos produtos em Data

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,10 +1,17 @@
+import iphone11 from "./assets/images/products/iphone11.png"
+import iphone11pro from "./assets/images/products/iphone11_pro_max.png"
+import iphone12mini from "./assets/images/products/iphone12_mini.png"
+import iphone12 from "./assets/images/products/iphone12.png"
+import iphone12pro from "./assets/images/products/iphone12_proo.png"
+import iphone12promax from "./assets/images/products/iphone11_pro_max.png"
+
 const DATA = [
     {
         id:0,
         title:'IPHONE 11',
         price:'750',
         desc:'O iPhone 11 é um smartphone da Apple lançado em 2019. Ele possui uma tela LCD de 6,1 polegadas, câmera dupla traseira de 12 MP, processador A13 Bionic e suporte para Face ID. Ele também possui resistência à água e resistência à poeira. ',
-        img:"../assets/images/products/iphone11.png"
+        img:iphone11
 
     },
     {
@@ -12,7 +19,7 @@ const DATA = [
         title:'IPHONE 11 Pro',
         price:'800',
         desc:'O iPhone 11 Pro é um smartphone da Apple lançado em 2019. Ele possui uma tela OLED de 5,8 polegadas, processador A13 Bionic, câmeras traseiras triplas de 12 MP, resistência à água e poeira e suporte para carregamento sem fio. Ele foi o primeiro iPhone a ter uma câmera traseira tripla, incluindo uma lente ultra-wide, uma lente wide e uma lente telephoto. Ele também possui uma bateria de longa duração e vem com o sistema operacional iOS 13.',
-        img:'../public/assets/images/products/iphone11_pro_max.png'
+        img:iphone11pro
 
     },
     {
@@ -20,7 +27,7 @@ const DATA = [
         title:'IPHONE 12 mini',
         price:'950',
         desc:'O iPhone 12 mini é um smartphone da Apple lançado em 2020. Ele possui uma tela OLED de 5,4 polegadas, processador A14 Bionic, câmeras traseiras duplas de 12 MP, resistência à água e poeira e suporte para carregamento sem fio e carregamento MagSafe. Ele vem com o sistema operacional iOS 14 e suporta a tecnologia 5G. Ele é considerado uma versão compacta do iPhone 12, oferecendo as mesmas especificações e recursos, mas em um corpo menor e mais fácil de segurar.',
-        img:'../public/assets/images/products/iphone12_mini.png'
+        img:iphone12mini
 
     },
     {
@@ -28,7 +35,7 @@ const DATA = [
         title:'IPHONE 12',
         price:'1050',
         desc:'O iPhone 12 é um smartphone da Apple lançado em 2020. Ele possui uma tela OLED de 6,1 polegadas, processador A14 Bionic, câmeras traseiras duplas de 12 MP, resistência à água e poeira e suporte para carregamento sem fio e carregamento MagSafe. Ele vem com o sistema operacional iOS 14 e suporta a tecnologia 5G. Ele é considerado como uma atualização do iPhone 11, com melhorias significativas na tela, câmera e desempenho geral. Além disso, ele também tem uma estrutura de cerâmica e vidro que o torna mais resistente.',
-        img:'../public/assets/images/products/iphone12.png'
+        img:iphone12
 
     },
     {
@@ -36,7 +43,7 @@ const DATA = [
         title:'IPHONE 12 pro',
         price:'1150',
         desc:'O iPhone 12 Pro é um smartphone da Apple lançado em 2020. Ele possui uma tela OLED de 6,1 polegadas, processador A14 Bionic, câmeras traseiras triplas de 12 MP, resistência à água e poeira e suporte para carregamento sem fio e carregamento MagSafe. Ele vem com o sistema operacional iOS 14 e suporta a tecnologia 5G. Ele é considerado como uma versão mais avançada do iPhone 12, oferecendo recursos adicionais como câmera LIDAR, gravação de vídeo ProRes e HDR, além de opções de armazenamento maiores. Ele também tem uma estrutura de cerâmica e vidro que o torna mais resistente.',
-        img:'../public/assets/images/products/iphone12_proo.png'
+        img:iphone12pro
 
     },
     {
@@ -44,7 +51,7 @@ const DATA = [
         title:'IPHONE 12 pro max',
         price:'1250',
         desc:'O iPhone 12 Pro Max é o topo de linha smartphone da Apple lançado em 2020. Ele possui uma tela OLED de 6,7 polegadas, processador A14 Bionic, câmeras traseiras triplas de 12 MP, resistência à água e poeira e suporte para carregamento sem fio e carregamento MagSafe. Ele vem com o sistema operacional iOS 14 e suporta a tecnologia 5G. Ele é considerado como a versão mais avançada e premium da linha iPhone 12, oferecendo recursos adicionais como câmera LIDAR, gravação de vídeo ProRes e HDR, além de opções de armazenamento maiores. Ele também tem uma estrutura de cerâmica e vidro que o torna mais resistente. Ele tem a maior tela dentre todos os modelos da linha iPhone 12 e a câmera principal possui estabilização óptica de imagem melhorada.',
-        img:'../public/assets/images/products/iphone12_pro_max.png'
+        img:iphone12promax
 
     }
 ]
